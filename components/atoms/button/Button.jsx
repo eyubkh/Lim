0c2eae50c81d2986dba649359790474b8fd6ebd1
eyubkh <eyub.kh@gmail.com>
2022-05-28 16:51:08 +0100

    build: adding styled components

--- components/atoms/button/Button.jsx
+++ components/atoms/button/Button.jsx
@@ -1,19 +1,15 @@
+import styled from 'styled-components'
+
+const Component = styled.button`
+  font-size: 16px;
+  padding: 8px 50px;
+  border-radius: 40px;
+  text-decoration: none;
+  border: none;
+  font-weight: 700;
+  background-color: #3cffc2;
+  cursor: pointer;
+`
 export default function Button({ text, handler }) {
-  return (
-    <>
-      <button onClick={handler}>{text}</button>
-      <style jsx>{`
-        button {
-          font-size: 16px;
-          padding: 8px 50px;
-          border-radius: 40px;
-          text-decoration: none;
-          border: none;
-          font-weight: 700;
-          background-color: #3cffc2;
-          cursor: pointer;
-        }
-      `}</style>
-    </>
-  )
+  return <Component onClick={handler}>{text}</Component>
 }
