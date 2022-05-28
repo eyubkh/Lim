6e224365ca6f67ab1cef00e319e473615c87fdc2
eyubkh <eyub.kh@gmail.com>
2022-05-28 16:53:47 +0100

    build: replacing styled component for normal tag

--- components/molecules/textField/TextField.jsx
+++ components/molecules/textField/TextField.jsx
@@ -1,27 +1,23 @@
 import styled from 'styled-components'
 import DisplayText from '../../atoms/displayText/DisplayText'
 
-const Component = styled.div`
-  margin-bottom: 20px;
-`
 const Input = styled.input`
   width: 300px;
   border-radius: 40px;
   border: 1px solid #00000055;
   padding: 5px 10px;
   outline: none;
+  margin-top: 5px;
 `
 
 export default function TextField({ title, subTitle }) {
   return (
-    <Component>
-      <DisplayText size="regular" element="p">
-        {title}
-      </DisplayText>
+    <div>
+      <DisplayText size="regular">{title}</DisplayText>
       <Input />
-      <DisplayText size="small" element="p">
+      <DisplayText size="small" right>
         {subTitle}
       </DisplayText>
-    </Component>
+    </div>
   )
 }
