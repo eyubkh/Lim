49068847b4bce034208e4f4005f70be151c082a3
eyubkh <eyub.kh@gmail.com>
2022-05-31 16:12:53 +0100

    build: creating files apollo server api

---
+++ pages/api/graphql.js
@@ -0,0 +1,30 @@
+import { ApolloServer } from 'apollo-server-micro'
+import Cors from 'micro-cors'
+import resolvers from '../../graphql/resolvers'
+import typeDefs from '../../graphql/typeDefs'
+import connectDB from '../../utils/middleware/mongodb'
+
+const cors = Cors()
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+const startServer = apolloServer.start()
+
+const handler = cors(async (req, res) => {
+  if (req.method === 'OPTIONS') {
+    res.end()
+    return false
+  }
+  await startServer
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+})
+
+export default connectDB(handler)
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
