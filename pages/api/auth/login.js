6144fcbce70579d0fcc68165e46ea6656b407deb
eyubkh <eyub.kh@gmail.com>
2022-06-02 17:02:54 +0100

    build: adding auth api for login

---
+++ pages/api/auth/login.js
@@ -0,0 +1,27 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import errorHandler from '../../../middleware/errorHandler'
+import connectDB from '../../../middleware/mongodb'
+import User from '../../../models/user'
+
+const handler = async (request, response) => {
+  const { username, password } = request.body
+  try {
+    const { password: passHash } = await User.findOne({ username })
+    const isCorrect = await bcrypt.compare(password, passHash)
+    if (isCorrect) {
+      const object = {
+        username,
+        password: passHash,
+      }
+      const token = jwt.sign(object, process.env.JWT_KEY)
+      response.send({ jwt: token })
+    } else {
+      throw Error()
+    }
+  } catch (error) {
+    errorHandler(error, response)
+  }
+}
+
+export default connectDB(handler)
