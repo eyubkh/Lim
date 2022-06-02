546984c264624d787ccb63e15ce2ee51b76cf6eb
eyubkh <eyub.kh@gmail.com>
2022-06-02 11:34:43 +0100

    test: mocking apollo query

--- components/organisms/login/Login.test.jsx
+++ components/organisms/login/Login.test.jsx
@@ -1,10 +1,39 @@
+import { gql } from '@apollo/client'
+import { MockedProvider } from '@apollo/client/testing'
 import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
 import Login from './Login'
 
+const TEST = gql`
+  mutation Test($username: String) {
+    user(username: $username) {
+      username
+    }
+  }
+`
+
+const mocks = [
+  {
+    request: {
+      query: TEST,
+      variables: { username: 'ayub' },
+    },
+    result: {
+      data: {
+        user: { username: 'ayob' },
+      },
+    },
+  },
+]
+
 describe('<Login />', () => {
   test('mocking test pass', () => {
-    const component = render(<Login />)
+    const component = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Login />
+      </MockedProvider>
+    )
+
     expect(component.container).toHaveTextContent('Your social network.')
   })
 })
