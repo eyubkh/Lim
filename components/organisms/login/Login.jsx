6062a241764cac39f523698c251b9a91b4f1296e
eyubkh <eyub.kh@gmail.com>
2022-06-02 19:14:55 +0100

    build: adding fetch to api/auth, saving jwt in localstore, and redirect to home if success

--- components/organisms/login/Login.jsx
+++ components/organisms/login/Login.jsx
@@ -1,37 +1,38 @@
-import { gql, useMutation } from '@apollo/client'
+import { useRouter } from 'next/router'
 import Button from '../../atoms/button/Button'
 import DisplayText from '../../atoms/displayText/DisplayText'
 import StyleText from '../../atoms/styleText/StyleText'
 import TextField from '../../molecules/textField/TextField'
 
-const TEST = gql`
-  mutation Test($username: String) {
-    user(username: $username) {
-      username
-    }
-  }
-`
 export default function Login({ setForm }) {
-  const [login, { data, loading }] = useMutation(TEST)
+  const router = useRouter()
   const handler = (event) => {
     event.preventDefault()
     setForm('singup')
   }
   const formSubmitHandler = async (event) => {
     event.preventDefault()
-    const elements = [...event.target]
-    const inputs = elements.filter((input) => input.type !== 'submit')
-    const inputsData = inputs.map(({ type, value }) => {
-      if (type === 'text') type = 'username'
-      return { [type]: value }
-    })
-    const { username } = Object.assign(...inputsData)
-    login({ variables: { username } })
+    const object = {
+      username: event.target[0].value,
+      password: event.target[1].value,
+    }
+    const { jwt } = await window
+      .fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(object),
+      })
+      .then((response) => response.json())
+    if (jwt) {
+      window.localStorage.setItem('token', jwt)
+      router.push('/home')
+    }
   }
   return (
     <>
       <form onSubmit={formSubmitHandler}>
-        {data ? <p>username ${data?.user.password}</p> : <p> nada bro</p>}
         <h2>Your social network.</h2>
         <TextField type="text" title="User name or Email" />
         <TextField
