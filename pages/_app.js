3c25ce3e646a533e529796a152489ca9a9fb372e
eyubkh <eyub.kh@gmail.com>
2022-06-02 19:13:23 +0100

    style: removing comment

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ApolloProvider } from '@apollo/client'
 import client from '../apollo-client'
 import '../styles/globals.css'
-// This default export is required in a new `pages/_app.js` file.
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
