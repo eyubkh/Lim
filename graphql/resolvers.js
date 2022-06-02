2b0caa6984019b6d7b9588861451a751ff23c300
eyubkh <eyub.kh@gmail.com>
2022-06-02 16:58:37 +0100

    build: removing login typeDefs and resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,17 +1,8 @@
-import User from '../models/user'
-
 export default {
   Query: {
-    get() {
-      return { message: 'queeeeee' }
+    test() {
+      return 'test'
     },
   },
-  Mutation: {
-    async user(root, args, context) {
-      const { username } = args
-      const findOne = await User.findOne({ username: username })
-      if (findOne === null) throw new Error('Mi error me cago en dios')
-      return findOne
-    },
-  },
+  Mutation: {},
 }
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,18 +1,10 @@
 import { gql } from 'apollo-server-micro'
 
 export default gql`
-  type User {
-    username: String
-    password: String
+  type Login {
+    jwt: String
   }
-  type Get {
-    message: String
-  }
-
   type Query {
-    get: Get
-  }
-  type Mutation {
-    user(username: String): User
+    test: String
   }
 `
