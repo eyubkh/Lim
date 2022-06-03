0c3e03b11a3d5bed806e7a0518bf490660257749
eyubkh <eyub.kh@gmail.com>
2022-06-03 18:54:06 +0100

    build: adding sidebar and user wall

--- pages/home.js
+++ pages/home.js
@@ -1,23 +1,36 @@
-import { gql, useQuery } from '@apollo/client'
 import { useRouter } from 'next/router'
+import styled from 'styled-components'
+import CreatePost from '../components/organisms/createPost/CreatePost'
+import Post from '../components/organisms/post/Post'
+import SideBar from '../components/organisms/sideBar/SideBar'
 
-const TEST = gql`
-  query Test {
-    test
-  }
+const Component = styled.div`
+  display: grid;
+  grid-template-columns: 35% 1fr;
+  height: 100vh;
+`
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 44px;
+  padding: 72px 26px;
 `
 
 export default function Home() {
-  const { data } = useQuery(TEST)
   const router = useRouter()
   const logOutHandler = (event) => {
     window.localStorage.removeItem('token')
     router.push('/')
   }
   return (
-    <>
-      <p>Welcome to home user!</p>
-      <button onClick={logOutHandler}> log out</button>
-    </>
+    <Component>
+      <SideBar username={'Eyubkh'} />
+      <Section>
+        <CreatePost />
+        <Post text={'Dia de cachimba'} username={'Abdulah'} likeCount={12} />
+        <button onClick={logOutHandler}>log out</button>
+      </Section>
+    </Component>
   )
 }
