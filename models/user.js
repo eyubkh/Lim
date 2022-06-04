d6e3bb2f27ee31523cbff296a6798b9f5a323d0c
eyubkh <eyub.kh@gmail.com>
2022-06-04 16:19:27 +0100

    build: changing schema, adding posts schema logic

--- models/user.js
+++ models/user.js
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
+  profile: {
+    username: String,
+    password: String,
   },
-  password: String,
+  posts: [
+    {
+      text: String,
+      likes: Number,
+      time: String,
+      username: String,
+    },
+  ],
 })
 
 export default mongoose.models.User || mongoose.model('User', userSchema)
