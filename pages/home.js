b37210d318e7265d6f78f2244a92c4ee2e60ff31
eyubkh <eyub.kh@gmail.com>
2022-06-04 16:21:15 +0100

    build: adding query user for user info and handling loading, error, success state

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,7 @@
+import { gql, useQuery } from '@apollo/client'
 import { useRouter } from 'next/router'
 import styled from 'styled-components'
+import client from '../apollo-client'
 import CreatePost from '../components/organisms/createPost/CreatePost'
 import Post from '../components/organisms/post/Post'
 import SideBar from '../components/organisms/sideBar/SideBar'
@@ -8,6 +10,7 @@ const Component = styled.div`
   display: grid;
   grid-template-columns: 35% 1fr;
   height: 100vh;
+  position: relative;
 `
 
 const Section = styled.section`
@@ -17,19 +20,62 @@ const Section = styled.section`
   padding: 72px 26px;
 `
 
+export const GET_USER = gql`
+  query user {
+    user {
+      profile {
+        id
+        username
+      }
+      posts {
+        id
+        username
+        likes
+        text
+        time
+      }
+    }
+  }
+`
+
+function getStringTimeIat(iat) {
+  const now = Math.round((Date.now() - iat) / 60000)
+  return `${Math.round(now / 60)}h ago`
+}
+
 export default function Home() {
+  const { data, loading, error } = useQuery(GET_USER)
   const router = useRouter()
-  const logOutHandler = (event) => {
+  const logOutHandler = async (event) => {
     window.localStorage.removeItem('token')
+    await client.clearStore()
     router.push('/')
   }
+  if (error) {
+    window.localStorage.removeItem('token')
+    router.push('/')
+    return <>error</>
+  }
+  if (loading) return <p>Cagando...</p>
   return (
     <Component>
-      <SideBar username={'Eyubkh'} />
+      <SideBar username={data?.user.profile.username} />
       <Section>
         <CreatePost />
-        <Post text={'Dia de cachimba'} username={'Abdulah'} likeCount={12} />
         <button onClick={logOutHandler}>log out</button>
+        {data?.user.posts
+          .map((post) => {
+            return (
+              <Post
+                key={post.id}
+                text={post.text}
+                iat={getStringTimeIat(post.time)}
+                username={post.username}
+                likeCount={post.likes}
+              />
+            )
+          })
+          .reverse()}
       </Section>
     </Component>
   )
