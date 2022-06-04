7edc31f2cd50f0e0a9179fc28e3b557fa0788df1
eyubkh <eyub.kh@gmail.com>
2022-06-04 16:14:09 +0100

    build: creating logic for create post

--- components/organisms/createPost/CreatePost.jsx
+++ components/organisms/createPost/CreatePost.jsx
@@ -1,14 +1,28 @@
+import { gql, useMutation } from '@apollo/client'
 import styled from 'styled-components'
+import { GET_USER } from '../../../pages/home'
 import Icon from '../../atoms/icon/Icon'
+const CREATE_POST = gql`
+  mutation createPost($text: String) {
+    createPost(text: $text) {
+      text
+    }
+  }
+`
 
-const Component = styled.div`
+const Component = styled.form`
   background-color: #f3f3f3;
   border-radius: 26px;
   padding: 26px 44px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-
+  button {
+    border: none;
+    outline: none;
+    background-color: #f3f3f3;
+    cursor: pointer;
+  }
   input {
     background-color: transparent;
     border: none;
@@ -17,10 +31,21 @@ const Component = styled.div`
 `
 
 export default function CreatePost({}) {
+  const [createPost] = useMutation(CREATE_POST, {
+    refetchQueries: [{ query: GET_USER }, 'user'],
+  })
+  const sumbitHandler = (event) => {
+    event.preventDefault()
+    createPost({ variables: { text: event.target[0].value } })
+    event.target[0].value = ''
+  }
+
   return (
-    <Component>
+    <Component onSubmit={sumbitHandler}>
       <input placeholder="What's happening?" />
-      <Icon icon={'add'} />
+      <button>
+        <Icon icon={'add'} />
+      </button>
     </Component>
   )
 }
