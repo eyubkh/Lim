4e231b81063aa4af640862f380abc16f1f640cad
eyubkh <eyub.kh@gmail.com>
2022-06-04 16:15:30 +0100

    build: adding posts and profile typeDefs and resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,27 @@
+import { UserInputError } from 'apollo-server-micro'
+import User from '../models/user'
 export default {
   Query: {
-    test(root, args, ctx) {
-      console.log(ctx)
+    async user(root, args, ctx) {
       if (!ctx) return null
-      return 'test'
+      const user = await User.findOne({ _id: ctx.id })
+      if (!user) throw new UserInputError('not user found')
+      return user
+    },
+  },
+  Mutation: {
+    async createPost(root, args, ctx) {
+      if (!ctx) throw new UserInputError('please login')
+      const user = await User.findOne({ _id: ctx.id })
+      const newPost = {
+        text: args.text,
+        likes: 0,
+        time: Date.now(),
+        username: user.profile.username,
+      }
+      user.posts = user.posts.concat(newPost)
+      await user.save().then(() => console.log('post saved', newPost))
+      return newPost
     },
   },
 }
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,7 +1,26 @@
 import { gql } from 'apollo-server-micro'
 
 export default gql`
+  type Profile {
+    id: ID
+    username: String
+    password: String
+  }
+  type Post {
+    id: ID
+    text: String
+    time: String
+    likes: Int
+    username: String
+  }
+  type User {
+    profile: Profile
+    posts: [Post]
+  }
   type Query {
-    test: String
+    user: User
+  }
+  type Mutation {
+    createPost(text: String): Post
   }
 `
