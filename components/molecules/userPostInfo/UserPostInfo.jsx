69200fe45c37aa4feb230cf6b1c0b2b02840ae4a
eyubkh <eyub.kh@gmail.com>
2022-06-09 18:05:43 +0100

    build: adding like logic and image post

--- components/molecules/userPostInfo/UserPostInfo.jsx
+++ components/molecules/userPostInfo/UserPostInfo.jsx
@@ -1,5 +1,7 @@
+import { useMutation } from '@apollo/client'
 import Image from 'next/image'
 import styled from 'styled-components'
+import { GET_USER, LIKE } from '../../../utils/queries'
 import DisplayText from '../../atoms/displayText/DisplayText'
 import Icon from '../../atoms/icon/Icon'
 
@@ -7,6 +9,11 @@ const Component = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  button {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+  }
 `
 
 const UserPostAvatar = styled.div`
@@ -18,27 +25,26 @@ const UserPostAvatar = styled.div`
   }
 `
 
-const myLoader = ({ src, width, quality }) => {
-  return `https://${src}/${width}`
-}
-
-export default function UserPostInfo({ username, iat, likeCount }) {
+export default function UserPostInfo({ username, image, id, iat, likeCount }) {
+  const [like] = useMutation(LIKE, {
+    refetchQueries: [{ query: GET_USER }],
+  })
+  const likeHandler = (event) => {
+    event.preventDefault()
+    like({ variables: { likeid: id } })
+  }
   return (
     <Component>
       <UserPostAvatar>
-        <Image
-          loader={myLoader}
-          src={'picsum.photos/'}
-          width={44}
-          height={44}
-          alt={'post user avatar'}
-        />
+        <Image src={image} width={44} height={44} alt={'post user avatar'} />
         <section>
           <DisplayText size={'regular'}>{username}</DisplayText>
           <DisplayText size={'small'}>{iat}</DisplayText>
         </section>
       </UserPostAvatar>
-      <Icon icon={'like'} count={likeCount} />
+      <button onClick={likeHandler}>
+        <Icon icon={'like'} count={likeCount} />
+      </button>
     </Component>
   )
 }
