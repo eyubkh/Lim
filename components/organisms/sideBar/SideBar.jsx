155960992e0313cce0f026c5c45472679528048f
eyubkh <eyub.kh@gmail.com>
2022-06-09 18:07:21 +0100

    build: adding friendsearch logic and fixing how post is showing

--- components/organisms/sideBar/SideBar.jsx
+++ components/organisms/sideBar/SideBar.jsx
@@ -1,5 +1,9 @@
+import { useMutation } from '@apollo/client'
+import { useState } from 'react'
 import styled from 'styled-components'
+import { FRIEND_SEARCH } from '../../../utils/queries'
 import FriendAvatar from '../../atoms/friendAvatar/FriendAvatar'
+import FriendSearch from '../../molecules/friendSearch/FriendSearch'
 import UserAvatar from '../../molecules/userAvatar/UserAvatar'
 
 const Component = styled.div`
@@ -7,12 +11,42 @@ const Component = styled.div`
   background-color: #f3f3f3;
 `
 
-export default function SideBar({ username }) {
+export default function SideBar({ username, imagePath, friends }) {
+  const [searching, { data }] = useMutation(FRIEND_SEARCH)
+  const [search, setSearch] = useState(false)
+  const onChangeHandler = (event) => {
+    event.preventDefault()
+    searching({ variables: { text: event.target.value } })
+  }
   return (
     <Component>
-      <UserAvatar username={username} />
-      <h3>Friend</h3>
-      <FriendAvatar username={'Abdulah'} />
+      <UserAvatar username={username} imagePath={imagePath} />
+      <FriendSearch
+        search={search}
+        setSearch={setSearch}
+        handler={onChangeHandler}
+      />
+      {search
+        ? data?.friendSearch.map((friend) => {
+            return (
+              <FriendAvatar
+                id={friend.id}
+                key={friend.id}
+                username={friend.username}
+                image={friend.imgPath}
+              />
+            )
+          })
+        : friends?.map((friend) => {
+            return (
+              <FriendAvatar
+                id={friend.id}
+                key={friend.id}
+                username={friend.username}
+                image={friend.imgPath}
+              />
+            )
+          })}
     </Component>
   )
 }
