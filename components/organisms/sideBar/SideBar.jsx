2808ab3d2b029ade72c33435ca5775e9be3f5fbd
eyubkh <eyub.kh@gmail.com>
2022-06-10 19:38:09 +0100

    build: adding new style and adding some logic

--- components/organisms/sideBar/SideBar.jsx
+++ components/organisms/sideBar/SideBar.jsx
@@ -2,13 +2,24 @@ import { useMutation } from '@apollo/client'
 import { useState } from 'react'
 import styled from 'styled-components'
 import { FRIEND_SEARCH } from '../../../utils/queries'
-import FriendAvatar from '../../atoms/friendAvatar/FriendAvatar'
+import FriendAvatar from '../../molecules/friendAvatar/FriendAvatar'
 import FriendSearch from '../../molecules/friendSearch/FriendSearch'
 import UserAvatar from '../../molecules/userAvatar/UserAvatar'
 
 const Component = styled.div`
-  padding: 44px 26px;
-  background-color: #f3f3f3;
+  position: relative;
+  width: 300px;
+  .sideBar {
+    width: 300px;
+    position: fixed;
+    display: grid;
+    gap: 17px;
+    padding: 44px 0px 44px 26px;
+    .friendList {
+      display: grid;
+      gap: 17px;
+    }
+  }
 `
 
 export default function SideBar({ username, imagePath, friends }) {
@@ -20,33 +31,39 @@ export default function SideBar({ username, imagePath, friends }) {
   }
   return (
     <Component>
-      <UserAvatar username={username} imagePath={imagePath} />
-      <FriendSearch
-        search={search}
-        setSearch={setSearch}
-        handler={onChangeHandler}
-      />
-      {search
-        ? data?.friendSearch.map((friend) => {
-            return (
-              <FriendAvatar
-                id={friend.id}
-                key={friend.id}
-                username={friend.username}
-                image={friend.imgPath}
-              />
-            )
-          })
-        : friends?.map((friend) => {
-            return (
-              <FriendAvatar
-                id={friend.id}
-                key={friend.id}
-                username={friend.username}
-                image={friend.imgPath}
-              />
-            )
-          })}
+      <div className="sideBar">
+        <UserAvatar username={username} imagePath={imagePath} />
+        <FriendSearch
+          search={search}
+          setSearch={setSearch}
+          handler={onChangeHandler}
+        />
+        <div className="friendList">
+          {search
+            ? data?.friendSearch.map((friend) => {
+                return (
+                  <FriendAvatar
+                    id={friend.id}
+                    key={friend.id}
+                    username={friend.username}
+                    imagePath={friend.imgPath}
+                    isFriend={false}
+                  />
+                )
+              })
+            : friends?.map((friend) => {
+                return (
+                  <FriendAvatar
+                    id={friend.id}
+                    key={friend.id}
+                    username={friend.username}
+                    imagePath={friend.imgPath}
+                    isFriend={true}
+                  />
+                )
+              })}
+        </div>
+      </div>
     </Component>
   )
 }
