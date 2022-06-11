b1cf81f91e0376c97801160cb0410ca703031bde
eyubkh <eyub.kh@gmail.com>
2022-06-11 19:08:06 +0100

    build: adding UI error provider, global context

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { ApolloProvider } from '@apollo/client'
 import client from '../apollo-client'
 import '../styles/globals.css'
+import { ErrorHandlerProvider } from '../utils/errorHandlerUI'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <ErrorHandlerProvider>
+        <Component {...pageProps} />
+      </ErrorHandlerProvider>
     </ApolloProvider>
   )
 }
