30d8ac12ce42136659b4c7760addf3ef9df0b32d
eyubkh <eyub.kh@gmail.com>
2022-06-11 20:09:52 +0100

    build: changing env names

--- utils/s3.js
+++ utils/s3.js
@@ -1,9 +1,9 @@
 import { S3 } from '@aws-sdk/client-s3'
 
 export const client = new S3({
-  region: process.env.AWS_REGION,
+  region: process.env.REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
   },
 })
