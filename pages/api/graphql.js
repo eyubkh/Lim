995c6d4006f05d5ac6ec245528375884f42f0a61
eyubkh <eyub.kh@gmail.com>
2022-06-11 20:54:20 +0100

    build: changing cors

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,12 +1,11 @@
 import { ApolloServer } from 'apollo-server-micro'
 import jwt from 'jsonwebtoken'
-import Cors from 'micro-cors'
+// import Cors from 'micro-cors'
 import resolvers from '../../graphql/resolvers'
 import typeDefs from '../../graphql/typeDefs'
 import connectDB from '../../middleware/mongodb'
 
-const cors = Cors()
-
+// const cors = Cors()
 async function getToken(token) {
   token = token.split(' ')[1]
   const object = jwt.verify(token, process.env.JWT_KEY)
@@ -29,7 +28,27 @@ export const config = {
   },
 }
 
-const handler = cors(async (req, res) => {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = async (req, res) => {
   if (req.method === 'OPTIONS') {
     res.end()
     return false
@@ -38,6 +57,17 @@ const handler = cors(async (req, res) => {
   await apolloServer.createHandler({
     path: '/api/graphql',
   })(req, res)
-})
+}
 
-export default connectDB(handler)
+// const handler = cors(async (req, res) => {
+//   if (req.method === 'OPTIONS') {
+//     res.end()
+//     return false
+//   }
+//   await startServer
+//   await apolloServer.createHandler({
+//     path: '/api/graphql',
+//   })(req, res)
+// })
+
+export default connectDB(allowCors(handler))
