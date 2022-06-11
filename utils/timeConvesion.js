9dc821a49aa95b82d2d1da7e4b51636093ea293f
eyubkh <eyub.kh@gmail.com>
2022-06-11 19:04:09 +0100

    build: adding complexity to time conversion

--- utils/timeConvesion.js
+++ utils/timeConvesion.js
@@ -1,4 +1,11 @@
 export default function getStringTimeIat(iat) {
-  const now = Math.round((Date.now() - iat) / 60000)
-  return `${Math.round(now / 60)}h ago`
+  const now = Math.round(Math.floor((Date.now() - iat) / 60000) / 60)
+  if (now < 1) {
+    return 'now'
+  } else if (now < 24) {
+    return `${now}h ago`
+  } else {
+    const time = Math.round(Math.floor(now / 24))
+    return `${time} day${time > 1 ? 's' : ''} ago`
+  }
 }
