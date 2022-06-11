a5164ca99318fb39910cfd5495a7f8dc25faeb80
eyubkh <eyub.kh@gmail.com>
2022-06-11 19:12:46 +0100

    build: removing unused error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,4 @@
 export default function (error, response) {
-  console.log(error.message, error.name)
-  if (error.name === 'TypeError') {
-    return response.status(401).json({ message: 'Wrong credentials' })
-  }
-  return response.status(500).json({ message: error })
+  console.log(error.fileName, error.name)
+  return response.status(500).json({ message: error.message })
 }
