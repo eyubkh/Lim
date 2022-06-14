8ada1f694c2872586afceb6f5d20369d65e03131
eyubkh <eyub.kh@gmail.com>
2022-06-14 16:06:18 +0100

    build: adding send icon

--- components/atoms/icon/Icon.mocks.js
+++ components/atoms/icon/Icon.mocks.js
@@ -2,4 +2,12 @@ export const mocksIconProps = {
   icon: 'default',
 }
 
-export const iconRules = ['default', 'like', 'add', 'search']
+export const iconRules = [
+  'default',
+  'like',
+  'add',
+  'search',
+  'comment',
+  'send',
+  'user',
+]
