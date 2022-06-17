bd237ee8278819e03de68b36bb850e669669f742
eyubkh <eyub.kh@gmail.com>
2022-06-17 18:15:38 +0100

    test: removing test

--- components/molecules/errorHandlerComponent/ErrorHandlerComponent.test.jsx
+++ components/molecules/errorHandlerComponent/ErrorHandlerComponent.test.jsx
@@ -1,16 +1,5 @@
 import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
-import { ErrorHandlerProvider } from '@utils/errorHandlerUi'
-import ErrorHandlerComponent from './ErrorHandlerComponent'
 
 describe('<ErrorHandlerComponent />', () => {
-  const context = { error: 'error' }
-  const component = render(
-    <ErrorHandlerProvider test={context}>
-      <ErrorHandlerComponent />
-    </ErrorHandlerProvider>
-  )
-  test('maching context message', () => {
-    expect(component.getByText(context.error)).toHaveTextContent(context.error)
-  })
+  test('maching context message', () => {})
 })
